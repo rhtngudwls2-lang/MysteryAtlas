bfe1f466effd41f370561533837b987c26b97c66
rhtngudwls2-lang <rhtngudwls2@gmail.com>
2026-09-12 12:20:02 +0900

    Create MainActivity.kt

---
+++ app/src/main/java/org/mysteryatlas/MainActivity.kt
@@ -0,0 +1,9 @@
+package org.mysteryatlas
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import org.mysteryatlas.ui.*
+class MainActivity:ComponentActivity() {
+ override fun onCreate(savedInstanceState:Bundle?) {super.onCreate(savedInstanceState);enableEdgeToEdge();setContent {AtlasTheme {AtlasApp()}}}
+}
